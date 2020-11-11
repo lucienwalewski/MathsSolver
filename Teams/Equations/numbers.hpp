e00a1c2e4eb72face346d72f3dbfe33646225654
ypxrain <ypxrain@outlook.com>
2020-11-11 23:09:38 +0100

    add class complex

--- Teams/Equations/numbers.hpp
+++ Teams/Equations/numbers.hpp
@@ -1,4 +1,46 @@
+class Complex
+{
+public:
+    Complex(double r, double i)
+    {
+        this->r = r;
+        this->i = i;
+    }
+    double get_r()
+    {
+        return r;
+    }
+    double get_i()
+    {
+        return i;
+    }
+    Complex operator + (const Complex &a)
+    {
+        return Complex(r + a.r, i + a.i);
+    }
+    Complex operator - (const Complex &a)
+    {
+        return Complex(r - a.r, i - a.i);
+    }
+    Complex operator * (const Complex &a)
+    {
+        return Complex(r * a.r - i * a.i, r * a.i + i * a.r);
+    }
+    Complex operator / (const double &a)
+    {
+        return Complex(r / a, i / a);
+    }
+    Complex conj()
+    {
+        return Complex(r, -i);
+    }
+private:
+    double i, r;
+};
+
 int gcd(int a, int b);
 bool Miller_Rabin(long long x, long long p);
 long long power(long long x, long long y, long long p);
 long long mul_with_mod(unsigned long long x, unsigned long long y, long long p);
+
+void FFT(Complex *y, int len, bool flag);
