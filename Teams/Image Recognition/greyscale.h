94febfea7ab9598caecb4b57a0bdf404d206f080
lucienwalewski <lucien.walewski@polytechnique.edu>
2020-11-12 09:57:22 +0100

    Add greyscale class for preprocessing

---
+++ Teams/Image Recognition/greyscale.h
@@ -0,0 +1,14 @@
+//
+// Created by Lucien Walewski on 12/11/2020.
+//
+
+#ifndef MATHSSOLVER_GREYSCALE_H
+#define MATHSSOLVER_GREYSCALE_H
+
+
+class greyscale {
+
+};
+
+
+#endif //MATHSSOLVER_GREYSCALE_H
