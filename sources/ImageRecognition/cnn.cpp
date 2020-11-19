925f9728fa476e3c8e97731b354bb15098fe4052
lucienwalewski <lucien.walewski@polytechnique.edu>
2020-11-19 12:15:39 +0100

    Updated cnn

--- sources/ImageRecognition/cnn.cpp
+++ sources/ImageRecognition/cnn.cpp
@@ -1,10 +1,19 @@
+#pragma once
 #include "cnn.hpp"
 #include <Eigen/Dense>
 
 using namespace Eigen;
 using namespace std;
 
-CNN::CNN()
-{
+/* This function takes two 2d matrices of the same dimensions,
+ * convolves them and adds a bias */
+double CNN::conv_single_step(MatrixXd slice_prev, MatrixXd W, double b) {
+    MatrixXd conv = slice_prev.cwiseProduct(W);
+    double Z = conv.sum();
+    Z += b;
+    return Z;
+}
 
+MatrixXd CNN::zero_pad(MatrixXd image, int pad) {
+    return image;
 }
--- sources/ImageRecognition/cnn.hpp
+++ sources/ImageRecognition/cnn.hpp
@@ -1,5 +1,11 @@
 #ifndef CNN_HPP
+#pragma once
 #define CNN_HPP
+
+#include <Eigen/Dense>
+
+using namespace Eigen;
+using namespace std;
 
 
 class CNN
@@ -11,9 +17,11 @@ public:
     void info();
 
 private:
+    double conv_single_step(MatrixXd slice_prev, MatrixXd W, double b);
     int forward_prop();
     int backword_prop();
     double cross_entropy();
+    MatrixXd zero_pad(MatrixXd image, int pad);
 };
 
 #endif // CNN_HPP
