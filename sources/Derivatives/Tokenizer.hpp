87948102f7b2ceb2ccabf804bcb7de5b4babce61
marie-huynh <marie.huynhmilan@gmail.com>
2020-12-06 16:44:38 +0100

    tokenizer hpp + tokenizer cpp
    restructuring of the two files

--- sources/Derivatives/Tokenizer.hpp
+++ sources/Derivatives/Tokenizer.hpp
@@ -18,29 +18,11 @@ enum operator_type {
     pare = 7};
 
 
-bool is_letter(char s){
-    if ((s>='0' && s<='9') or  ( (s>='A' && s<='Z'))){
-        return true;
-    }
-    return false;
-};
+bool is_letter(char s);
 
-bool is_number(char s){
-    if ((s>='0' && s<='9')){
-        return true;
-    }
-    return false;
-};
+bool is_number(char s);
 
-bool is_in_vector(vector< string > v, string elem){
-    return (find(v.begin(), v.end(), elem) != v.end());
-};
-
-vector <string> constants{"pi", "e"};
-
-vector <string> functions{"cos", "sin", "exp", "tan", "sqrt", "ln"};
-
-vector <string> str_operators{"+", "-", "/", "^", "sqrt", "(", ")"};
+bool is_in_vector(vector< string > v, string elem);
 
 
 class Token {
@@ -48,18 +30,10 @@ protected :
     string value;
     operator_type type;
 public :
-    Token(string s){
-        value = s;
-     }
-    Token(){
-        value = "";
-    }
-    string get_value(){
-            return value;
-        }
-    operator_type get_type(){
-        return type;
-    }
+    Token(string s);
+    Token();
+    string get_value();
+    operator_type get_type();
 };
 
 
@@ -69,110 +43,30 @@ private :
     operator_type type;
 
 public :
-    Operator():Token(){
-        type = none;
-    }
-    Operator(string t): Token(t){
-        if (t == "+"){
-            type = addition;
-
-        };
-        if (t == "-"){
-            type = sub;
-        };
-        if (t == "*"){
-            type = mul;
-        };
-        if (t == "/"){
-            type = divi;
-        };
-        if (t == "^"){
-            type = power;
-
-        };
-        if (t == "~"){
-                type = comp;
-            };
-        if (t == "(" or t == ")"){
-            type = pare;
-        };
-        if (t == ""){
-            type = none;
-        }
-    };
-
-    Operator(operator_type t){
-        type = t;
-    }
-
-    operator_type get_type(){
-        return type;
-    }
-
-
+    Operator();
+    Operator(string t);
+    Operator(operator_type t);
+    operator_type get_type();
 };
 
 
 class Num: public Token {
 public :
-    Num(string t):Token(t){
-        }
+    Num(string t):Token(t){};
 };
 
 class Function: public Token{
 public :
-    Function(string t): Token(t){
-        }
+    Function(string t): Token(t){};
 };
 
 class Variable: public Token{
 public:
-    Variable(string t): Token(t){
-    }
+    Variable(string t): Token(t){};
 };
 
 
-vector<Token> simplify(string s, char variable){
-    vector <Token> new_vector;
-    string current;
-    //std::string::iterator i = s.begin();
-    for (int i=0; i < int(s.size()); i++){
-        if ((s[i] == ' ') || (s[i] == '\t') || (s[i] == '\n')){
-            continue;
-        }
-        else if ((s[i] == variable) || (!is_letter(s[i])) ){
-            if (is_in_vector(constants, current)){
-                new_vector.push_back(Num(current));
-                current = "";
-            }
-            else if (is_in_vector(functions, current)){
-                new_vector.push_back(Function(current));
-                current = "";
-            }
-            else {
-                new_vector.push_back(Num(current));
-                current = "";
-            }
-            if (is_in_vector(str_operators, string(1,s[i]))){
-
-                string c(1, s[i]);
-                new_vector.push_back(Operator(c));
-            }
-            else if (is_number(s[i])){
-                string c(1, s[i]);
-                new_vector.push_back(Num(c));
-            }
-            else if (s[i] == variable){
-                string c(1, s[i]);
-                new_vector.push_back(Variable(c));
-                }
-         }
-        else {
-             current.push_back(s[i]);
-    }
-}
-    return new_vector;
-};
+vector<Token> simplify(string s, char variable);
 
 
 #endif // TOKENIZER_HPP
