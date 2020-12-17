48b51bacde7594a6cf1597748c6a73874cdb22e9
Lemmy00 <64690721+Lemmy00@users.noreply.github.com>
2020-12-17 10:38:44 +0100

    Cleaning main

--- sources/main.cpp
+++ sources/main.cpp
@@ -2,13 +2,11 @@
 #include <QtTest>
 
 //#include "MainWindow/mainwindow.h"
-//#include "Derivatives/derivatives.hpp"
-#include "ImageRecognition/imagePreprocessing.hpp"
+//#include "ImageRecognition/imagePreprocessing.hpp"
 #include "Derivatives/Tokenizer.hpp"
 #include "NumericalSolver/num_derivation.hpp"
 #include "NumericalSolver/num_integration.hpp"
 #include "NumericalSolver/num_roots.hpp"
-
 //#include "Derivatives/derivatives.hpp"
 #include "Equations/polynomial.hpp"
 #include "Equations/matrix.hpp"
@@ -19,16 +17,8 @@
 #include<string>
 #include<math.h>
 #include<vector>
-//#include<opencv2/opencv.hpp>
-
-
 
 using namespace std;
-using namespace cv;
-
-/*double F(double x){
-    return sin(2*x-2.1)/(x*x+1);
-}*/
 
 int main(int argc, char *argv[])
 {
@@ -37,6 +27,10 @@ int main(int argc, char *argv[])
      string f;
      cout<<"Hello MathSolver!"<<" Enter the function we are solving: ";
      f = upload_function();
+     long long h[5] = {1, -1, -2, 1, 1};
+     int d = 4;
+     Polynomial p(h, d);
+
 
 
 //    MainWindow w;
