7879e216a96e2250f374f491e223be979e137ef8
aurelebohbot <aurelemmb@gmail.com>
2020-12-25 19:27:47 +0100

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,14 +1,15 @@
 #include <QApplication>
 #include <QtTest>
+//#include "mainwindow.h"
 
 //#include "MainWindow/mainwindow.h"
-//#include "Derivatives/derivatives.hpp"
+#include "Derivatives/derivatives.hpp"
 //#include "ImageRecognition/imagePreprocessing.hpp"
-//#include "Derivatives/Tokenizer.hpp"
+#include "Derivatives/Tokenizer.hpp"
 #include "NumericalSolver/num_derivation.hpp"
 #include "NumericalSolver/num_integration.hpp"
 #include "NumericalSolver/num_roots.hpp"
-//#include "Derivatives/derivatives.hpp"
+#include "Derivatives/derivatives.hpp"
 #include "Equations/polynomial.hpp"
 #include "Equations/matrix.hpp"
 #include "Equations/gaussin.hpp"
@@ -24,14 +25,10 @@
 
 
 using namespace std;
-//using namespace cv;
-
-/*double F(double x){
-    return sin(2*x-2.1)/(x*x+1);
-}*/
 
 int main(int argc, char *argv[])
 {
+<<<<<<< HEAD
     Matrix A(3, 3), Y(3, 1);
     A.set_element(0, 0, 2);
     A.set_element(0, 1, 5);
@@ -83,4 +80,22 @@ int main(int argc, char *argv[])
     gaussian(B, X);
 
     return 0;
+=======
+
+
+    QApplication  a(argc, argv);
+    string function = "";
+    //uncomment next line and make your own (simple for now) equation
+    //function = "sin(x+5)^3 + 3";
+    vector<Token> function_ex = simplify(function,'x');
+    AF Function = AF(function_ex);
+    //uncomment next line to get a back to string representation of your function tree
+    cout << "Function: "<<Function.display() << '\n';
+
+
+
+    //w.show();
+    return a.exec();
+
+>>>>>>> c39f93aebb920357cd11e08b082146ed97f88c21
 }
