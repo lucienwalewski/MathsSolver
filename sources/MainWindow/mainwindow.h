047e9ed7307773891e4fa29be868a7de57e8eefb
hugodemascarel <72524914+hugodemascarel@users.noreply.github.com>
2020-12-27 19:14:24 +0100

    Update mainwindow.h

--- sources/MainWindow/mainwindow.h
+++ sources/MainWindow/mainwindow.h
@@ -3,10 +3,11 @@
 
 #include <QMainWindow>
 #include <QPushButton>
-#include "inputbox.h"
 #include <QFileDialog>
 #include <QGroupBox>
+#include <QLineEdit>
 #include <QLabel>
+#include <QVBoxLayout>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -26,17 +27,38 @@ private slots:
 
 private:
     Ui::MainWindow *ui;
+
+//1. Title
     QLabel *title0;
     QLabel *title1;
     QGroupBox *group_0();
-    QGroupBox *first_group();
-    QGroupBox *second_group();
-    QLabel* info;
-    QPushButton* upload_file;
-    QPushButton* enter;
-//InputBox input_box;
+//2. Manual input
     QLabel * question;
     QLineEdit * equation_input;
-    QFileDialog * input_file;
+    QPushButton* enter;
+    QGroupBox *first_group();
+//3. Upload file
+    QLabel* info;
+    QPushButton* upload_file;
+    QGroupBox *second_group();
+//4. Steps
+    QLabel* label1;
+    QLabel* label2;
+    QLabel* label3;
+    QLabel* step1;
+    QLabel* step2;
+    QLabel* step3;
+    QGroupBox *group_step1();
+    QGroupBox *group_step2();
+    QGroupBox *group_step3();
+//5. Final Result
+    QLabel* output;
+    QLabel* result;
+    QGroupBox *group_result();
+//6. Main Layout
+    QVBoxLayout* main_layout;
+
+
 };
 #endif // MAINWINDOW_H
+
