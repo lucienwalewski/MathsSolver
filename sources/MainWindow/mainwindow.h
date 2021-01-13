44258ad85a5809da1bbb1b1e2c3e7106335c8b31
hugodemascarel <72524914+hugodemascarel@users.noreply.github.com>
2021-01-13 15:37:17 +0100

    Update mainwindow.h

--- sources/MainWindow/mainwindow.h
+++ sources/MainWindow/mainwindow.h
@@ -1,3 +1,25 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPushButton>
+#include <QFileDialog>
+#include <QGroupBox>
+#include <QLineEdit>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QTextEdit>
+#include <QKeyEvent>
+#include "functionPreprocessing.hpp"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
