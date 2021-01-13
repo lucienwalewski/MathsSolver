f58101f62cd1b0caa58c3fc698db5133256d97d8
hugodemascarel <72524914+hugodemascarel@users.noreply.github.com>
2021-01-13 15:00:00 +0100

    Update mainwindow.h

--- sources/MainWindow/mainwindow.h
+++ sources/MainWindow/mainwindow.h
@@ -1,24 +1,3 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
-
-#include <QMainWindow>
-#include <QPushButton>
-#include <QFileDialog>
-#include <QGroupBox>
-#include <QLineEdit>
-#include <QLabel>
-#include <QVBoxLayout>
-#include <QTextEdit>
-#include "functionPreprocessing.hpp"
-
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
-
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
-
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
@@ -31,42 +10,22 @@ private:
     Ui::MainWindow *ui;
 
 //1. Title
-    QLabel *title0;
-    QLabel *title1;
-    QGroupBox *group_0();
+    QGroupBox *title_group();
+
 //2. Manual input
-    QLabel * question;
-    QLineEdit * equation_input;
-    QPushButton* enter;
-    QGroupBox *first_group();
+    QLineEdit *equation_input;
+    QPushButton *confirm;
+    QGroupBox *manual_input_group();
 //3. Upload file
-    QLabel* info;
     QPushButton* upload_file;
-    QGroupBox *second_group();
-//4. Steps
-    QLabel* label1;
-    QLabel* label2;
-    QLabel* label3;
-    QWidget* steps;
-    QTextEdit* step1;
-    QTextEdit* step2;
-    QTextEdit* step3;
-    /*
-    QGroupBox *gs1;
-    QGroupBox *gs2;
-    QGroupBox *gs3;
-    QGroupBox *group_step1();
-    QGroupBox *group_step2();
-    QGroupBox *group_step3();
-    */
-//5. Final Result
-    QLabel* output;
-    QLabel* result;
-    QGroupBox *group_result();
-//6. Main Layout
+    QGroupBox *upload_group();
+
+    QGroupBox *final;
+//6. Layout
     QVBoxLayout* main_layout;
     QVBoxLayout* top_layout;
-    QVBoxLayout* steps_layout;
+    QWidget* steps_layoutf(vector<string>);
+    QGroupBox* results_layoutf(vector<string>);
     QHBoxLayout* results_layout;
 
 
